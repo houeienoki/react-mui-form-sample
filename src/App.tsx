0154752b4b45d11736f424437f533af9a90ee484
houeienoki <houeienoki@outlook.jp>
2022-01-03 01:38:35 +0900

    first commit.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { Button, TextField } from '@mui/material';
+import { useForm } from 'react-hook-form';
+
+
+type Inputs = {
+  name: string
+}
 
 function App() {
+  const { register, handleSubmit } = useForm<Inputs>();
+  const [ name, setName ] = useState<string>("");
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TextField 
+        id="name"
+        value={name}
+        {...register("name")}
+        onChange={(event) => setName(event.target.value)}
+      />
+      <Button
+        variant='contained'
+        onClick={() => {
+          setName("aaa");
+        }}>
+        値変更
+      </Button>
+      <Button
+        variant='contained'
+        onClick={handleSubmit((data) => {
+          console.log(data);
+        })}>
+        SUBMIT
+      </Button>
     </div>
   );
 }
